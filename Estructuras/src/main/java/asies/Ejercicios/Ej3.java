e74d1638f216988f6a842e894c197b05d76e52cf
A607\1DAW <davalorib@gmail.com>
2025-03-11 17:20:32 +0100

    subo 3 ejercicios del stack este

---
+++ Estructuras/src/main/java/asies/Ejercicios/Ej3.java
@@ -0,0 +1,30 @@
+package asies.Ejercicios;
+
+import java.util.Stack;
+
+public class Ej3 {
+
+    public static void main(String[] args) {
+
+        Stack<String> pila = new Stack<>();
+
+        String si = "(2+3)*(5-1)";
+
+        for (String token : si.split("")) {
+            if (token.equals("(")) {
+                pila.push(token);
+            } else if (token.equals(")")) {
+                if (pila.contains("(")){
+                    pila.pop();
+                }
+            }
+        }
+
+        if (pila.isEmpty()){
+            System.out.println("Ta bien");
+        } else {
+            System.out.println("Ta mal");
+        }
+
+    }
+}
--- Estructuras/src/main/java/asies/Main.java
+++ Estructuras/src/main/java/asies/Main.java
@@ -1,11 +1,15 @@
 package asies;
 
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        
+        ArrayList<Integer> pila = new ArrayList<>();
+
+
 
     }
 
